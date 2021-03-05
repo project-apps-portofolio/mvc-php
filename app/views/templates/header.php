be92d21418d64ac56a20c8f29d97d5f1d9e71e00
ilyas <ilyas@mewahniagajaya.com>
2021-03-05 10:28:49 +0700

    add ui on home index

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -5,6 +5,9 @@
   <title></title>
   <link rel="stylesheet" href="<?= BASEURL; ?>/css/bootstrap.css">
   <link rel="stylesheet" href="<?= BASEURL; ?>/css/custome.css">
+  <link rel="stylesheet" href="<?= BASEURL; ?>/css/placeholder-loading.min.css">
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
 </head>
 
 <body>
@@ -18,7 +21,6 @@
         <div class="navbar-nav">
           <a class="nav-item nav-link" href="<?= BASEURL; ?>/order">Order</a>
           <a class="nav-item nav-link" href="<?= BASEURL; ?>/category">Category</a>
-
         </div>
       </div>
     </div>
